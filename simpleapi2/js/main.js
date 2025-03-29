1f94ddadfd63ba3cb9a84d15808a337b82f13d4b
Francelie <francelie.ovalle@gmail.com>
2025-03-29 09:43:55 -0400

    updated js and index

--- simpleapi2/js/main.js
+++ simpleapi2/js/main.js
@@ -1,1 +1,26 @@
 
+document.querySelector('button').addEventListener('click', letsCook)
+
+function letsCook (){
+let ingredients = document.querySelector('input').value
+fetch (`https://www.themealdb.com/api/json/v1/1/search.php?s=${ingredients}`)
+.then (res => res.json())
+.then (data => {
+    console.log(data.meals[0]);
+    let embed = data.meals[0].strYoutube.replace('watch?v=', 'embed/')
+    document.querySelector('h2').innerText = data.meals[0].strMeal
+    document.querySelector('iframe').src = embed
+    //document.querySelector('h3').innerText = data.meals[0].strInstructions
+    
+    
+})
+.catch (error => {
+   console.log(`error ${error}`);
+    
+})
+}
+
+//Note to self: how to embed video
+//https://www.youtube.com/embed/OfM7VlonD5c?rel=0
+//https://www.youtube.com/watch?v=2sX4fCgg-UI
+//https://www.youtube.com/embed/2sX4fCgg-UI
